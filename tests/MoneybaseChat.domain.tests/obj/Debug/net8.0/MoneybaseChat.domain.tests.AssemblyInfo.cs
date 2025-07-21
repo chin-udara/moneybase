0c0bd1c1bc7fb67ee673c92e35972ecb33c8a64b
Udara Laptop <udara@thesoftwarefirm.lk>
2025-07-21 19:30:24 +0530

    test: added tests for application and infrastructure

--- tests/MoneybaseChat.domain.tests/obj/Debug/net8.0/MoneybaseChat.domain.tests.AssemblyInfo.cs
+++ tests/MoneybaseChat.domain.tests/obj/Debug/net8.0/MoneybaseChat.domain.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneybaseChat.domain.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bfa3e7303403624b615ff3c57c137d76ba4699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06fc120d236bb9bd745ab5775b56ffb386c971f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneybaseChat.domain.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneybaseChat.domain.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
